88fb858d11369f1986705cc712b769fae6dce9e9
Britain Smith <Britain.D.Smith@gmail.com>
2021-12-20 10:29:50 -0600

    set up correct mongo atlas connection with mikro-orm

--- dist/mikro-orm.config.js
+++ dist/mikro-orm.config.js
@@ -1,11 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongo_highlighter_1 = require("@mikro-orm/mongo-highlighter");
 const constants_1 = require("./constants");
 const Post_1 = require("./entities/Post");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const options = {
     type: "mongo",
     entities: [Post_1.Post],
+    clientUrl: process.env.MONGO_URI,
     dbName: "lireddit",
     highlighter: new mongo_highlighter_1.MongoHighlighter(),
     debug: constants_1.__prod__,
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -4,10 +4,14 @@ import { Options } from "@mikro-orm/core";
 import { MongoHighlighter } from "@mikro-orm/mongo-highlighter";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const options: Options = {
 	type: "mongo",
 	entities: [Post],
+	clientUrl: process.env.MONGO_URI,
 	dbName: "lireddit",
 	highlighter: new MongoHighlighter(),
 	debug: __prod__,
